f0077e7a98f74f716a7b85075fddf3da0b85a2d3
Cade Cuddy <cadecuddy@gmail.com>
2024-04-18 12:01:45 -0400

    Create config if it already doesn't exist

--- src/config.rs
+++ src/config.rs
@@ -16,15 +16,44 @@ pub struct Config {
 #[cfg(target_family = "unix")]
 pub fn get_config_directory() -> PathBuf {
     match env::var("XDG_CONFIG_HOME") {
-        Ok(var) => Path::new(&var).join("spotisnatch").to_path_buf(),
-        Err(_) => Path::new(&env::var("HOME").unwrap()).join(".config/spotisnatch"),
+        Ok(var) => Path::new(&var).join("spotisync").to_path_buf(),
+        Err(_) => Path::new(&env::var("HOME").unwrap()).join(".config/spotisync"),
     }
 }
 
 impl Config {
+    fn create_config() -> Result<(), ConfigError> {
+        let config_directory_path = get_config_directory();
+        let config_file_path = config_directory_path.join("config.json");
+        // create config directory if it doesn't exist
+        if !config_directory_path.exists() {
+            std::fs::create_dir_all(&config_directory_path)?;
+        }
+        // create config file if it doesn't exist
+        if !config_file_path.exists() {
+            let default_config = Config {
+                username: String::new(),
+                client_id: String::new(),
+                client_secret: String::new(),
+            };
+            let default_config_string = serde_json::to_string_pretty(&default_config)?;
+            std::fs::write(config_file_path, default_config_string)?;
+        }
+        Ok(())
+    }
+
     pub fn load_config() -> Result<Config, ConfigError> {
         let config_directory_path = get_config_directory();
         let config_file_path = config_directory_path.join("config.json");
+
+        if !config_file_path.exists() {
+            Config::create_config()?;
+            println!(
+                "Config file didn't already exist, please fill in config file at {:?}",
+                config_file_path
+            );
+            std::process::exit(1);
+        }
 
         // attempt to read config file
         let config_file_string = read_to_string(config_file_path)?;
